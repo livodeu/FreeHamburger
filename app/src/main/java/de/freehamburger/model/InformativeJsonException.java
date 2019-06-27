f30051923c9226aae225ecf40e32fd4d9e780364
livodeu <livodeu@gmail.com>
2019-06-27 10:41:08 +0200

    initial commit

---
+++ app/src/main/java/de/freehamburger/model/InformativeJsonException.java
@@ -0,0 +1,40 @@
+package de.freehamburger.model;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.JsonReader;
+import android.util.MalformedJsonException;
+
+import java.io.IOException;
+
+/**
+ * Wraps a MalformedJsonException and the state of the JsonReader that caused it.
+ */
+public class InformativeJsonException extends IOException {
+
+    @Nullable private final String readerState;
+
+    /**
+     * Constructor.
+     * @param base MalformedJsonException that has been thrown
+     * @param reader JsonReader
+     */
+    InformativeJsonException(MalformedJsonException base, @Nullable JsonReader reader) {
+        super(base.getMessage());
+        this.readerState = reader != null ? reader.toString() : null;
+    }
+
+    @Nullable
+    public String getReaderState() {
+        return this.readerState;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        if (this.readerState != null) {
+            return super.toString() + " - " + this.readerState;
+        }
+        return super.toString();
+    }
+}
