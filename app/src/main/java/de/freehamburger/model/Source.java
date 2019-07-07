8f4f5f58a419b4df37a3f0adb2f50f1794e25654
livodeu <livodeu@gmail.com>
2019-07-07 11:02:12 +0200

    replaced iconForSource() with getIcon()

--- app/src/main/java/de/freehamburger/model/Source.java
+++ app/src/main/java/de/freehamburger/model/Source.java
@@ -17,22 +17,24 @@ import de.freehamburger.R;
  */
 public enum Source {
 
-    HOME(R.string.action_section_homepage, App.URL_PREFIX + "homepage/"),
-    NEWS(R.string.action_section_news, App.URL_PREFIX + "news/"),
-    INLAND(R.string.action_section_inland, App.URL_PREFIX + "news/?ressort=inland"),
-    AUSLAND(R.string.action_section_ausland, App.URL_PREFIX + "news/?ressort=ausland"),
-    SPORT(R.string.action_section_sport, App.URL_PREFIX + "news/?ressort=sport"),
-    VIDEO(R.string.action_section_video, App.URL_PREFIX + "news/?ressort=video"),
-    WIRTSCHAFT(R.string.action_section_wirtschaft, App.URL_PREFIX + "news/?ressort=wirtschaft"),
-    CHANNELS(R.string.action_section_channels, App.URL_PREFIX + "channels/"),
+    HOME(R.string.action_section_homepage, App.URL_PREFIX + "homepage/", R.drawable.ic_home_black_24dp),
+    NEWS(R.string.action_section_news, App.URL_PREFIX + "news/", R.drawable.ic_local_library_black_24dp),
+    INLAND(R.string.action_section_inland, App.URL_PREFIX + "news/?ressort=inland", R.drawable.ic_germany),
+    AUSLAND(R.string.action_section_ausland, App.URL_PREFIX + "news/?ressort=ausland", R.drawable.ic_language_black_24dp),
+    SPORT(R.string.action_section_sport, App.URL_PREFIX + "news/?ressort=sport", R.drawable.ic_directions_run_black_24dp),
+    VIDEO(R.string.action_section_video, App.URL_PREFIX + "news/?ressort=video", R.drawable.ic_videocam_black_24dp),
+    WIRTSCHAFT(R.string.action_section_wirtschaft, App.URL_PREFIX + "news/?ressort=wirtschaft", R.drawable.ic_build_black_24dp),
+    CHANNELS(R.string.action_section_channels, App.URL_PREFIX + "channels/", R.drawable.ic_tv_black_24dp),
     /** params look like "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16" */
-    REGIONAL(R.string.action_section_regional, App.URL_PREFIX + "news/?regions=", true)
+    REGIONAL(R.string.action_section_regional, App.URL_PREFIX + "news/?regions=", R.drawable.ic_place_black_24dp, true)
     ;
 
     @StringRes
     private final int label;
     private final String url;
     private final boolean needsParams;
+    @DrawableRes
+    private final int icon;
 
     /**
      * Returns the parameter char sequence for {@link #REGIONAL}, based on the user's preferences.
@@ -60,33 +62,12 @@ public enum Source {
     }
 
     /**
-     * Returns an icon for a given Source.
-     * @param source  Source
-     * @return drawable resource
-     */
-    @DrawableRes
-    public static int iconForSource(@NonNull final Source source) {
-        switch (source) {
-            case HOME: return R.drawable.ic_home_black_24dp;
-            case NEWS: return R.drawable.ic_local_library_black_24dp;
-            case SPORT: return R.drawable.ic_directions_run_black_24dp;
-            case AUSLAND: return R.drawable.ic_language_black_24dp;
-            case VIDEO: return R.drawable.ic_videocam_black_24dp;
-            case INLAND: return R.drawable.ic_germany;
-            case CHANNELS: return R.drawable.ic_tv_black_24dp;
-            case REGIONAL: return R.drawable.ic_place_black_24dp;
-            case WIRTSCHAFT: return R.drawable.ic_build_black_24dp;
-        }
-        return 0;
-    }
-
-    /**
      * Constructor.
      * @param label string resource id
      * @param url url
      */
-    Source(@StringRes int label, String url) {
-        this(label, url, false);
+    Source(@StringRes int label, String url, @DrawableRes int icon) {
+        this(label, url, icon, false);
     }
 
     /**
@@ -95,10 +76,20 @@ public enum Source {
      * @param url url
      * @param needsParams {@code true} if this Source needs additional parameters
      */
-    Source(@StringRes int label, String url, boolean needsParams) {
+    Source(@StringRes int label, String url, @DrawableRes int icon, boolean needsParams) {
         this.label = label;
         this.url = url;
+        this.icon = icon;
         this.needsParams = needsParams;
+        if (icon == 0) throw new java.lang.AssertionError("No source icon");
+    }
+
+    /**
+     * @return icon for this Source
+     */
+    @DrawableRes
+    public int getIcon() {
+        return icon;
     }
 
     /**
