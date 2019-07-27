a3e175fb69ee197a0b2617a78e4d83b6ac2db807
livodeu <livodeu@gmail.com>
2019-07-27 12:34:34 +0200

    initial commit

---
+++ app/src/main/java/de/freehamburger/NewsAdapterActivity.java
@@ -0,0 +1,9 @@
+package de.freehamburger;
+
+import de.freehamburger.adapters.NewsRecyclerAdapter;
+
+/**
+ * HamburgerActivity that implements NewsRecyclerAdapter.NewsAdapterController.
+ */
+public abstract class NewsAdapterActivity extends HamburgerActivity implements NewsRecyclerAdapter.NewsAdapterController {
+}
