03a04f4984459fc291ab280b7efee406bc11f6ad
livodeu <livodeu@gmail.com>
2019-09-01 12:09:35 +0200

    added copyright attribute to Item

--- app/src/main/java/de/freehamburger/model/Gallery.java
+++ app/src/main/java/de/freehamburger/model/Gallery.java
@@ -1,6 +1,7 @@
 package de.freehamburger.model;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.VisibleForTesting;
 import android.text.TextUtils;
 import android.util.JsonReader;
@@ -56,6 +57,7 @@ public class Gallery implements Serializable {
 
         private final Map<Quality, String> images = new HashMap<>(3);
         private String title;
+        private String copyright;
 
         static Item parse(@NonNull final JsonReader reader) throws IOException {
             Item item = new Item();
@@ -74,7 +76,9 @@ public class Gallery implements Serializable {
                     item.title = reader.nextString();
                 } else if ("alttext".equals(name)) {
                     String alttext = reader.nextString();
-                    if (!TextUtils.isEmpty(alttext)) item.title = alttext;
+                    if (TextUtils.isEmpty(item.title)) item.title = alttext;
+                } else if ("copyright".equals(name)) {
+                    item.copyright = reader.nextString();
                 } else if ("videowebs".equals(name)) {
                     reader.beginObject();
                     reader.nextName();
@@ -91,7 +95,6 @@ public class Gallery implements Serializable {
                     item.addImage(Quality.L, reader.nextString());
                     reader.endObject();
                 } else {
-                    //if (BuildConfig.DEBUG) android.util.Log.i(Gallery.Item.class.getSimpleName(), "Skipping " + name);
                     reader.skipValue();
                 }
             }
@@ -101,6 +104,11 @@ public class Gallery implements Serializable {
 
         private void addImage(Quality quality, String url) {
             this.images.put(quality, url);
+        }
+
+        @Nullable
+        public String getCopyright() {
+            return copyright;
         }
 
         @NonNull
