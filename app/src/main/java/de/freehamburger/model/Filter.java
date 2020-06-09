de02d44b7307bc72ba935ad69aede6619eb2809f
livodeu <livodeu@gmail.com>
2020-06-09 12:14:01 +0200

    added one javadoc comment and removed one SuppressWarnings

--- app/src/main/java/de/freehamburger/model/Filter.java
+++ app/src/main/java/de/freehamburger/model/Filter.java
@@ -7,7 +7,11 @@ import androidx.annotation.Nullable;
  */
 public interface Filter extends Comparable<Filter> {
 
-    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+    /**
+     * Returns {@code true} if the News object should be included, {@code false} if it should be omitted.
+     * @param news News
+     * @return true / false
+     */
     boolean accept(@Nullable final News news);
 
     CharSequence getText();
