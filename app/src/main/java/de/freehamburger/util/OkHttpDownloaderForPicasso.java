7d4406bf12b46c9dc6b2f8e099799466d37a973f
livodeu <livodeu@gmail.com>
2022-12-04 14:36:56 +0100

    catch IllegalStateExceptions

--- app/src/main/java/de/freehamburger/util/OkHttpDownloaderForPicasso.java
+++ app/src/main/java/de/freehamburger/util/OkHttpDownloaderForPicasso.java
@@ -3,9 +3,11 @@ package de.freehamburger.util;
 import android.content.Context;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 
 import androidx.annotation.NonNull;
 import de.freehamburger.App;
+import de.freehamburger.BuildConfig;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -32,7 +34,15 @@ public class OkHttpDownloaderForPicasso implements com.squareup.picasso.Download
     @NonNull
     @Override
     public Response load(@NonNull Request request) throws IOException {
-        return this.client.newCall(request).execute();
+        try {
+            // on an emulator instance (API 31) with exoplayer 2.18.2 there once was
+            // E/HamburgerService: Loading image from 'https://www.tagesschau.de/…/….jpg' failed: java.lang.IllegalStateException: cache is closed
+            // but this could not be reproduced
+            return this.client.newCall(request).execute();
+        } catch (IllegalStateException ise) {
+            if (BuildConfig.DEBUG) Log.e(getClass().getSimpleName(), ise.toString(), ise);
+            return new Response.Builder().code(HttpURLConnection.HTTP_INTERNAL_ERROR).message(ise.toString()).request(request).build();
+        }
     }
 
     /** {@inheritDoc} */
