88970d80e81a6e761c37f4cbd502364f9e5ee8ff
livodeu <livodeu@gmail.com>
2023-01-06 15:52:01 +0100

    if Intro cannot run, try again at the next opportunity

--- app/src/main/java/de/freehamburger/util/Intro.java
+++ app/src/main/java/de/freehamburger/util/Intro.java
@@ -1,21 +1,26 @@
 package de.freehamburger.util;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Handler;
 
 import androidx.annotation.IntRange;
 import androidx.annotation.NonNull;
+import androidx.preference.PreferenceManager;
 
 import java.lang.ref.Reference;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.freehamburger.App;
+
 /**
  * Executes several Runnables one after the other.
  */
 public class Intro implements Runnable {
 
+    private final App app;
     private final Reference<Activity> refAct;
     private final Handler handler = new Handler();
     private final List<Runnable> steps = new ArrayList<>();
@@ -26,9 +31,11 @@ public class Intro implements Runnable {
     /**
      * Constructor.
      * @param activity Activity
+     * @throws NullPointerException if {@code activity} is {@code null}
      */
     public Intro(@NonNull Activity activity) {
         super();
+        this.app = (App)activity.getApplicationContext();
         this.refAct = new WeakReference<>(activity);
     }
 
@@ -42,6 +49,16 @@ public class Intro implements Runnable {
         this.handler.removeCallbacksAndMessages(null);
     }
 
+    /**
+     * Sets the appropriate flag in the preferences to enable the Intro.
+     */
+    private void enable() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this.app);
+        SharedPreferences.Editor ed = prefs.edit();
+        ed.putBoolean(App.PREF_PLAY_INTRO, true);
+        ed.apply();
+    }
+
     public boolean isPlaying() {
         return this.playing;
     }
@@ -49,12 +66,19 @@ public class Intro implements Runnable {
     @Override
     public void run() {
         Activity activity = this.refAct.get();
-        if (activity == null) return;
+        if (activity == null) {
+            enable();
+            return;
+        }
         this.playing = true;
         long total = 0L;
         final int nSteps = this.steps.size();
         for (int i = 0; i < nSteps; i++) {
-            if (this.cancelRequested || activity.isFinishing() || activity.isDestroyed()) break;
+            if (this.cancelRequested || activity.isFinishing() || activity.isDestroyed()) {
+                // if this happens, re-enable intro to run at the next opportunity
+                enable();
+                break;
+            }
             total += this.delays.get(i);
             this.handler.postDelayed(this.steps.get(i), total);
         }
