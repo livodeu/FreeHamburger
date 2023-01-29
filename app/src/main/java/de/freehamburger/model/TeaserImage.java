e7558177b1248d703909f546cc87af0350addea1
livodeu <livodeu@gmail.com>
2023-01-29 16:46:16 +0100

    use original16x9 if applicable

--- app/src/main/java/de/freehamburger/model/TeaserImage.java
+++ app/src/main/java/de/freehamburger/model/TeaserImage.java
@@ -1,7 +1,12 @@
 package de.freehamburger.model;
 
+import android.text.TextUtils;
 import android.util.JsonReader;
 import android.util.JsonToken;
+
+import androidx.annotation.IntDef;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.io.IOException;
 import java.io.Serializable;
@@ -9,22 +14,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.util.AbstractMap;
 import java.util.EnumMap;
-import java.util.Map;
-
-import androidx.annotation.IntDef;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 /**
  *
  */
 public class TeaserImage implements Serializable {
 
-    public static final int FORMAT_PORTRAIT = 1;
-    public static final int FORMAT_LANDSCAPE = 2;
-    public static final int FORMAT_SQUARE = 3;
+    @Format public static final int FORMAT_LANDSCAPE = 2;
+    @Format public static final int FORMAT_PORTRAIT = 1;
+    @Format public static final int FORMAT_SQUARE = 3;
     /** the {@link Quality Qualities} ordered from best to worst */
-    private static final Quality[] BEST_QUALITY = new Quality[] {Quality.L, Quality.P2, Quality.M, Quality.P1, Quality.S};
+    private static final Quality[] BEST_QUALITY = new Quality[]{Quality.L, Quality.P2, Quality.M, Quality.P1, Quality.S};
     final AbstractMap<Quality, String> images = new EnumMap<>(Quality.class);
     String title;
     String copyright;
@@ -41,13 +41,15 @@ public class TeaserImage implements Serializable {
      * Parses the given JsonReader to retrieve a TeaserImage element.
      * @param reader JsonReader
      * @return TeaserImage
-     * @throws IOException if an I/O error occurs
+     * @throws IOException          if an I/O error occurs
      * @throws NullPointerException if {@code reader} is {@code null}
      */
     @NonNull
     static TeaserImage parse(@NonNull final JsonReader reader) throws IOException {
         final TeaserImage teaserImage = new TeaserImage();
         reader.beginObject();
+        boolean videoweblFound = false;
+        String original16x9 = null;
         for (; reader.hasNext(); ) {
             String name = reader.nextName();
             JsonToken next = reader.peek();
@@ -79,8 +81,8 @@ public class TeaserImage implements Serializable {
                 reader.nextName();
                 teaserImage.addImage(Quality.L, reader.nextString());
                 reader.endObject();
+                videoweblFound = true;
             } else if ("portraetgrossplus8x9".equals(name)) {   // 512 x 576
-                // it seems that P2 is unreliable - although it is present, the corresponding download fails!
                 reader.beginObject();
                 reader.nextName();
                 teaserImage.addImage(Quality.P2, reader.nextString());
@@ -105,15 +107,24 @@ public class TeaserImage implements Serializable {
                 reader.nextName();
                 teaserImage.addImage(Quality.MQ, reader.nextString());
                 reader.endObject();
+            } else if ("original16x9".equals(name)) {
+                reader.beginObject();
+                reader.nextName();
+                original16x9 = reader.nextString();
+                reader.endObject();
             } else {
                 reader.skipValue();
             }
         }
         reader.endObject();
+        // "original16x9" seems to point to the same image url as "videowebl", so if "videowebl" wasn't here, use "original16x9" instead
+        if (!TextUtils.isEmpty(original16x9) && !videoweblFound) {
+            teaserImage.addImage(Quality.L, original16x9);
+        }
         return teaserImage;
     }
 
-    private void addImage(@NonNull Quality quality, String url) {
+    private void addImage(@NonNull Quality quality, @Nullable String url) {
         if (url == null) return;
         this.images.put(quality, url);
     }
@@ -137,7 +148,7 @@ public class TeaserImage implements Serializable {
 
     /**
      * Returns the image variant whose width is greater than the given width.
-     * @param width width in px
+     * @param width  width in px
      * @param format requested format ({@link #FORMAT_PORTRAIT}, {@link #FORMAT_LANDSCAPE}, {@link #FORMAT_SQUARE})
      * @return MeasuredImage
      */
@@ -170,32 +181,6 @@ public class TeaserImage implements Serializable {
     }
 
     @Nullable
-    public String getCopyright() {
-        return this.copyright;
-    }
-
-    /**
-     * Attemps to return the url for an image of given quality levels.<br>
-     * If none is found, it is attempted to return the best available quality.
-     * @param qualities one or more quality levels
-     * @return image url
-     */
-    @Nullable
-    public String getImage(@Nullable Quality... qualities) {
-        if (qualities == null) return getBestImage();
-        for (Quality quality : qualities) {
-            String url = this.images.get(quality);
-            if (url != null) return url;
-        }
-        return getBestImage();
-    }
-
-    @NonNull
-    public Map<Quality, String> getImages() {
-        return this.images;
-    }
-
-    @Nullable
     public String getTitle() {
         return this.title;
     }
@@ -217,7 +202,7 @@ public class TeaserImage implements Serializable {
     }
 
     /**
-     * They are be ordered from smallest width to biggest width, but, if same width, from biggest height to smallest height
+     * They are ordered from smallest width to biggest width, but, if same width, from biggest height to smallest height
      */
     public enum Quality {
         /** 140x140 */ SQ(140, 140),
@@ -229,9 +214,18 @@ public class TeaserImage implements Serializable {
         /** 559x559 */ LQ(559, 559),
         /** 960x540 */ L(960, 540);
 
+        private static final Quality[] FOR_LANDSCAPE = new Quality[]{S, M, L};
+        private static final Quality[] FOR_PORTRAIT = new Quality[]{P1, P2};
+        private static final Quality[] FOR_SQUARE = new Quality[]{SQ, MQ, LQ};
+
         private final int width;
         private final int height;
 
+        /**
+         * Constructor.
+         * @param width  width
+         * @param height height
+         */
         Quality(int width, int height) {
             this.width = width;
             this.height = height;
@@ -240,28 +234,29 @@ public class TeaserImage implements Serializable {
         /**
          * @return Quality array ordered to prefer landscape images over portrait images
          */
-        private static Quality[] valuesForLandscape() {
-            return new Quality[] {S, M, L};
+        static Quality[] valuesForLandscape() {
+            return FOR_LANDSCAPE;
         }
 
         /**
          * @return Quality array ordered to prefer portrait images over landscape images
          */
-        private static Quality[] valuesForPortrait() {
-            return new Quality[] {P1, P2};
+        static Quality[] valuesForPortrait() {
+            return FOR_PORTRAIT;
         }
 
         /**
          * @return Quality array ordered to prefer square images
          */
-        private static Quality[] valuesForSquare() {
-            return new Quality[] {SQ, MQ, LQ};
+        static Quality[] valuesForSquare() {
+            return FOR_SQUARE;
         }
     }
 
     @Retention(RetentionPolicy.SOURCE)
     @IntDef({FORMAT_PORTRAIT, FORMAT_LANDSCAPE, FORMAT_SQUARE})
-    public @interface Format {}
+    public @interface Format {
+    }
 
     /**
      * Wraps an image url and the image dimensions into one object.
@@ -274,8 +269,8 @@ public class TeaserImage implements Serializable {
 
         /**
          * Constructor.
-         * @param url image url
-         * @param width image width
+         * @param url    image url
+         * @param width  image width
          * @param height image height
          */
         private MeasuredImage(String url, int width, int height) {
